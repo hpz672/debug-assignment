9e1751481191696475e97926aea04ff35a282e7f
dykler <1692387245@qq.com>
2020-06-26 21:42:30 +0800

    Task 9 1.c-5.c Debugged

--- 9/1.c
+++ 9/1.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
-int cal_step(int a[], int n){
+
+int cal_step(int a[], int n)
+{
     return a[n] = a[n - 2] + a[n - 3];
 }
-int main() {
-	int N;//总台阶数
+
+int main()
+{
+    int N;
     scanf("%d", &N);
-    int step[50];//step[N]N级台阶的方法数
+    int step[50];
     step[0] = 0;
     step[1] = 0;
     step[2] = 1;
     step[3] = 1;
-    for (int i = 4; i <= N; i++){
+    int i; 
+    for (i = 4; i <= N; i++) {
         step[i] = step[i - 2] + step[i - 3];
     }
     printf("%d", step[N]);
--- 9/3.c
+++ 9/3.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
-    long long int fun(int n)
-    {if(n==2)
-     return 1;
-    if(n==3)
-     return 1;
-    else
-     return fun(n-2)+fun(n-3);
+
+long long int fun(int n)
+{
+	if (n == 1) {
+		return 0; 
+	}
+    if (n == 2) {
+        return 1;
     }
-int main() {
-long long int n;
-    //int sum = 0;
-    
-    while(scanf("%d",&n)!=EOF){
-        
-    printf("%lld\n",fun(n));
+    if (n == 3) {
+        return 1;
+    } else {
+        return fun(n - 2) + fun(n - 3);
     }
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%lld", fun(n));
     return 0;
 }
