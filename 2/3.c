ba3deb11848678e7e7c8a3abec90220839a9e2ff
dykler <1692387245@qq.com>
2020-06-26 20:57:11 +0800

    Task 2 1.c-5.c Debugged

--- 2/3.c
+++ 2/3.c
@@ -1,48 +1,47 @@
 #include <stdio.h>
 #include <string.h>
-int main() {
-    int matrix_a[10][10];
-    int matrix_b[10][10];
-    int m;
-    int n;
-    scanf("%d%d",&m,&n);
-    int mat[m][m];
-    memset(mat,0,sizeof(mat));
+
+int main() 
+{
+	int matrix_a[10][10] = {0};
+    int matrix_b[10][10] = {0};
     int i;
     int j;
     int k;
-    for(i=0;i<m;i++)
-    {
-        for(j=0;j<n;j++)
-        {
-            scanf("%d",&matrix_a[i][j]);
+    int m = 0;
+    int n = 0;
+    scanf("%d%d", &m, &n);
+     
+    
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+        	scanf("%d", &matrix_a[i][j]);
+		}
+	}
+            
+            
+    for (j = 0; j < n; j++) {
+        for (i = 0; i < m; i++) {
+        	scanf("%d", &matrix_b[j][i]);
+		}
+	}
+            
+    int mat[m][m];
+    memset(mat, 0, sizeof(int) * m * m);   
+    for (i = 0; i < m; i++) {
+        for(j = 0; j < m; j++) {
+            for(k = 0; k < n; k++) {
+            	mat[i][j] = mat[i][j] + matrix_a[i][k] * matrix_b[k][j];	
+			}
         }
     }
-    for(j=0;j<n;j++)
-    {
-        for(i=0;i<m;i++)
-        {
-            scanf("%d",&matrix_a[j][i]);
-        }
-    }
-   for(i=0;i<m;i++)
-    {
-        for(j=0;j<m;j++)
-        {
-            for(k=0;k<n;k++)
-            {
-                mat[i][j]+=(matrix_a[i][k]*matrix_b[k][j]);
-            }
-        }
-    }
-    for(i=0;i<m;i++)
-    {
-        for(j=0;j<m;j++)
-        {
-            if(j==m-1)
-                printf("%d\n",mat[i][j]);
-            else
-                printf("%d ",mat[i][j]);
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < m; j++) {
+            if (j == m - 1) {
+                printf("%d\n", mat[i][j]);
+            } else {
+                printf("%d ", mat[i][j]);
+			} 
         }
     }
     return 0;
