c59f678c0b9b353088245106033240f66da386aa
Kong Weihan <2830524879@qq.com>
2020-06-27 15:20:48 +0800

    task10 6.c-10.c debugged

--- 10/9.c
+++ 10/9.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-int main() {
+
+int main()
+{
     int a;
     int b;
     int c;
-    scanf("%d",&a);
-    for(b == 2 ; b <= a ; b++){
-        for(c == 2 ; c <= b ; c++){
-           if(b % c == 0) {
-              break; 
-           }else{
-               printf("%d",b);
-           }
+    scanf("%d", &a);
+    for (b = 2; b <= a; b++) {
+        for (c = 2; c <= b; c++) {
+            if (b % c == 0) {
+                break;
+            }
         }
-        
+        if (b == c) {
+            printf("%d\n", b);
+        }
     }
+    
     return 0;
 }
