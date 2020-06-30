e06644db40bde621ee5040d1fbeae152f0a53453
dykler <67228043+dykler@users.noreply.github.com>
2020-06-30 17:49:38 +0800

    Format Modified

--- 14/1.c
+++ 14/1.c
@@ -7,13 +7,13 @@ int main()
     scanf("%d", &n);
     for (i = n; i >= 1; i--) {
     	for (j = i ; j >= 1; j--) {
-        	if (j > 1) {
-            	printf("%d", j);
-            	printf(" ");
-        	} else {
+            if (j > 1) {
+                printf("%d", j);
+                printf(" ");
+            } else {
             	printf("%d\n", j);
-         	}
-    	}
-	}
+            }
+        }
+    }
     return 0;
 }
