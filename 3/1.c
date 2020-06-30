5d9e8ecf2042f3e0fa821c82bfe1b6334e476e72
dykler <67228043+dykler@users.noreply.github.com>
2020-06-30 17:00:54 +0800

    Update 1.c
    Format Modified

--- 3/1.c
+++ 3/1.c
@@ -2,49 +2,49 @@
 
 int main()
 {
-	int matrix[100][100];
-	int m;
+    int matrix[100][100];
+    int m;
     int n;
-	int i, j;
- 	int x;
- 	scanf("%d %d", &m, &n);
- 	for (i = 0; i < m; i++) {
-   		for (j = 0; j < n; j++) {
-   			scanf("%d", &matrix[i][j]);
-  		}
- 	}
- 	x = 0;
- 	int sum = 0; 
- 	while (x <= (m - 1) / 2 && x <= (n - 1) / 2) {
-  		for (i = x; i <= n - 1 - x; i++) {
-    		printf("%d", matrix[x][i]);
-    		sum++;
-    		if(sum != n * m) {
-        		printf(" ");
-        	}
-  		}
-  		for (i = x + 1; i <= m - 2 - x; i++) {
-    		printf("%d", matrix[i][n - 1 - x]);
-    		sum++;
-    		if(sum != n * m) {
-        		printf(" ");
-        	} 
-  		}
-  		for (i = x; i <= n - 1 - x && m - 1 - x != x; i++) {
-    		printf("%d", matrix[m - 1 - x][n - 1 - i]);
-    		sum++;
-    		if(sum != n * m) { 
-        		printf(" ");
-        	} 
-  		}
-  		for (i = x + 1; i <= m - 2 - x && x != (n - 1 - x); i++) {
-    		printf("%d", matrix[m - 1 - i][x]);
-    		sum++;
-    		if(sum != n * m) { 
-        		printf(" ");
-        	} 
-  		}
-  		x++;
- 	}
- 	return 0;
+    int i, j;
+    int x;
+    scanf("%d %d", &m, &n);
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+   	    scanf("%d", &matrix[i][j]);
+        }
+    }
+    x = 0;
+    int sum = 0; 
+    while (x <= (m - 1) / 2 && x <= (n - 1) / 2) {
+        for (i = x; i <= n - 1 - x; i++) {
+            printf("%d", matrix[x][i]);
+            sum++;
+            if(sum != n * m) {
+                printf(" ");
+            }
+        }
+        for (i = x + 1; i <= m - 2 - x; i++) {
+            printf("%d", matrix[i][n - 1 - x]);
+            sum++;
+            if(sum != n * m) {
+                printf(" ");
+            } 
+        }
+        for (i = x; i <= n - 1 - x && m - 1 - x != x; i++) {
+            printf("%d", matrix[m - 1 - x][n - 1 - i]);
+            sum++;
+            if(sum != n * m) { 
+                printf(" ");
+            } 
+        }
+        for (i = x + 1; i <= m - 2 - x && x != (n - 1 - x); i++) {
+            printf("%d", matrix[m - 1 - i][x]);
+            sum++;
+            if(sum != n * m) { 
+                printf(" ");
+            } 
+        }
+        x++;
+     }
+     return 0;
 }
