9272d1bf4b0325936a5e9410ffee05fad9d89c0b
dykler <67228043+dykler@users.noreply.github.com>
2020-06-30 17:20:15 +0800

    Update 4.c
    Format Modified

--- 4/4.c
+++ 4/4.c
@@ -2,28 +2,28 @@
 
 int main()
 {
-	int n = 10;
-	int m;
-	int numbers[10];
-	int i, j, k;
+    int n = 10;
+    int m;
+    int numbers[10];
+    int i, j, k;
 
-	for (j = 0; j < 10; j++) {
-		scanf("%d", &numbers[j]);
-		for (k = 0; k < j; k++) {
-			if (numbers[k] < numbers[j]) {
-				m = numbers[k];
-				numbers[k] = numbers[j];
-				numbers[j] = m;
-			}
-		}
-	}
+    for (j = 0; j < 10; j++) {
+        scanf("%d", &numbers[j]);
+        for (k = 0; k < j; k++) {
+            if (numbers[k] < numbers[j]) {
+                m = numbers[k];
+                numbers[k] = numbers[j];
+                numbers[j] = m;
+            }
+        }
+    }
 
-	for (i = 0; i < 10; i++) {
-		printf("%d", numbers[i]);
-		if (i < 9) {
-			printf(" ");
-		}
-	}
+    for (i = 0; i < 10; i++) {
+        printf("%d", numbers[i]);
+        if (i < 9) {
+            printf(" ");
+        }
+    }
 
-	return 0;
+    return 0;
 }
