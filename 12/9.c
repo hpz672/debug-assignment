061ef3c6807ea6c621fb7a553802f5a9a4d9756c
hpz672 <67246348+hpz672@users.noreply.github.com>
2020-06-30 17:30:51 +0800

    Modified to solve the problems of precision loss

--- 12/9.c
+++ 12/9.c
@@ -3,13 +3,13 @@
 
 int main() 
 {
-	double r;
-	double c, s;
-	scanf("%lf", &r);
-	c = 2 * PI * r;
-	s = PI * r *r;
-	printf("%.3lf\n", c);
-	printf("%.3lf\n", s);
-	
-	return 0;
+    double r;
+    double c, s;
+    scanf("%lf", &r);
+    c = 2 * PI * r;
+    s = PI * r *r;
+    printf("%.3lf\n", c);
+    printf("%.3lf\n", s);
+    
+    return 0;
 }
