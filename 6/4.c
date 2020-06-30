c89ab7bffb2018827399460fa8b385d3fe219d61
dykler <67228043+dykler@users.noreply.github.com>
2020-06-30 17:33:50 +0800

    Format Modified

--- 6/4.c
+++ 6/4.c
@@ -12,47 +12,47 @@ w
 */
 int main() 
 {
-	char before[4] = {'\0'};
-    scanf("%s",before);
+    char before[4] = {'\0'};
+    scanf("%s", before);
     char calculate[1000][3] = {'\0'};
     int i;
     int count=0;
     int counter=0;
-    while (scanf("%s",calculate[count]) != EOF) { 
+    while (scanf("%s", calculate[count]) != EOF) { 
         count++;
     }
     for (i = 0; i < 3; i++) {
         if (before[i] == 'r') {
-			counter += 4;
-		}
+            counter += 4;
+        }
         if (before[i] == 'w') {
-			counter+=2;
-		}
+            counter += 2;
+        }
         if (before[i] == 'x') {
-			counter+=1;
-		}
+            counter += 1;
+        }
     }
     for (i = 0; i < count; i++) {
     	if (calculate[i][0] == '+') {
-            if (calculate[i][1]=='r' && ((counter&4) == 0)) {
-				counter+=4;
-			}
-            if (calculate[i][1]=='w' && ((counter&2) == 0)) {
-				counter+=2;
-			}
-            if (calculate[i][1]=='x' && ((counter&1) == 0)) {
-				counter+=1;
-			}
-        } else if (calculate[i][0]=='-') {
-            if (calculate[i][1]=='r' && ((counter&4) == 4)) {
-				counter-=4;
-			}
-            if (calculate[i][1]=='w' && ((counter&2) == 2)) {
-				counter-=2;
-			}
-            if (calculate[i][1]=='x' && ((counter&1) == 1)) {
-				counter-=1;
-			} 
+            if (calculate[i][1] == 'r' && ((counter & 4) == 0)) {
+                counter += 4;
+            }
+            if (calculate[i][1] == 'w' && ((counter & 2) == 0)) {
+                counter += 2;
+            }
+            if (calculate[i][1] == 'x' && ((counter & 1) == 0)) {
+                counter += 1;
+            }
+        } else if (calculate[i][0] == '-') {
+            if (calculate[i][1] == 'r' && ((counter & 4) == 4)) {
+                counter -= 4;
+            }
+            if (calculate[i][1] == 'w' && ((counter & 2) == 2)) {
+                counter -= 2;
+            }
+            if (calculate[i][1] == 'x' && ((counter & 1) == 1)) {
+                counter -= 1;
+            } 
         }  
     }
     printf("%d",counter);
