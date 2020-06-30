add74522d5420594d8e3872ccd9bca6b1196e5b0
hpz672 <67246348+hpz672@users.noreply.github.com>
2020-06-30 17:27:10 +0800

    Modified to solve the format problems

--- 10/8.c
+++ 10/8.c
@@ -2,19 +2,18 @@
 
 int main()
 {
-	int n, i, j;
-	scanf("%d", &n);
-	
-	for (i = 1; i <= n; i++) {
-		for (j = 2; j <= i; j++) {
-			if (i % j == 0) {
-				break;
-			}
-		}
-		if (j == i) {
-			printf("%d\n", i);
-		}
-	}
-
-	return 0;
+    int n, i, j;
+    scanf("%d", &n);
+    
+    for (i = 1; i <= n; i++) {
+        for (j = 2; j <= i; j++) {
+            if (i % j == 0) {
+                break;
+            }
+        }
+        if (j == i) {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
 }
