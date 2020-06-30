6f1184f69142182125dd7b1a638ff3b99aad0edf
hpz672 <67246348+hpz672@users.noreply.github.com>
2020-06-30 17:24:56 +0800

    Modified to solve the format problems

--- 9/10.c
+++ 9/10.c
@@ -8,17 +8,16 @@ output:31572
 */
 int f(int n) 
 {
-	if (n == 1) {
-		return 0;
-	}
-	if (n <= 3) {
+    if (n == 1) {
+        return 0;
+    }
+    if (n <= 3) {
         return 1;
     }
     else {
         return f(n - 2) + f(n - 3);
     }
 }
-
 
 int main()
 {
