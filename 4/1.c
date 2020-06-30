4de8ca3dfc7af8d2b383449e8db2aa83c02696d2
dykler <67228043+dykler@users.noreply.github.com>
2020-06-30 17:14:09 +0800

    Update 1.c
    Format Modified

--- 4/1.c
+++ 4/1.c
@@ -3,33 +3,33 @@
 
 int main()
 {
-	int n = 10;
-	int m;
-	int a[10];
-	int i;
+    int n = 10;
+    int m;
+    int a[10];
+    int i;
 
-	for (i = 0; i < n; i++) {
-		scanf("%d", &a[i]);
-	}
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+	
+    int temp = 0;
 
-	int temp = 0;
+    for (int i = 0; i < 10; i++) {
+        for (int j = i + 1; j < 10; j++) {
+            if (a[i] < a[j]) {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
 
-	for (int i = 0; i < 10; i++) {
-		for (int j = i + 1; j < 10; j++) {
-			if (a[i] < a[j]) {
-				temp = a[i];
-				a[i] = a[j];
-				a[j] = temp;
-			}
-		}
-	}
-
-	for (int i = 0; i < 10; i++) {
-		if(i != 9) {
-			printf("%d ", a[i]);
-		} else {
-			printf("%d", a[i]);
-		}
-	}
-	return 0;
+    for (int i = 0; i < 10; i++) {
+        if(i != 9) {
+            printf("%d ", a[i]);
+        } else {
+            printf("%d", a[i]);
+        }
+    }
+    return 0;
 }
