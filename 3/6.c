1082e363f1c5d3a79a89a106b5f78aaf770595a8
hpz672 <67246348+hpz672@users.noreply.github.com>
2020-06-30 17:44:51 +0800

    A little change in format

--- 3/6.c
+++ 3/6.c
@@ -12,82 +12,81 @@ output：
 */ 
 int main() 
 {
-	int matrix[100][100] = {0};
-	int m;
-	int n;
-	scanf("%d%d", &m, &n);
-	int start = 0, column = n, row = m, sum = 0;
+    int matrix[100][100] = {0};
+    int m;
+    int n;
+    scanf("%d%d", &m, &n);
+    int start = 0, column = n, row = m, sum = 0;
 
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
-			scanf("%d", &matrix[i][j]);
-		}
-	}
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
 
-	while(start < column && start < row) {
-		if (n == 1) {
-			for (int i = 0; i < m; i++) {
-				if (sum < m * n) {
-					printf("%d", matrix[i][0]);
-				}
-				sum ++;
-				if (sum < m * n) {
-					printf(" ");
-				}
-			}
-		} else if (m == 1) {
-			for (int j = 0; j < n; j++) {
-				printf("%d", matrix[0][j]);
-				sum ++;
-				if (sum < m * n) {
-					printf(" ");
-				}
-			}
-		} else {
-			for (int j = start; j < column; j++) {
-				if (sum < m * n) {
-					printf("%d", matrix[start][j]);
-					sum ++;
-				}
-				if (sum < m * n) {
-					printf(" ");
-				}
-			}
-
-			for (int i = start + 1; i < row; i++) {
-				if (sum < m * n) {
-					printf("%d", matrix[i][column-1]);
-					sum ++;
-				}
-				if (sum < m * n) {
-					printf(" ");
-				}
-			}
-
-			for (int j = column-2; j >= start; j--) {
-				if (sum < m*n) {
-					printf("%d", matrix[row-1][j]);
-					sum ++;
-				}
-				if (sum < m * n) {
-					printf(" ");
-				}
-			}
-
-			for (int i = row-2; i > start; i--) {
-				if (sum < m * n) {
-					printf("%d", matrix[i][start]);
-					sum ++;
-				}
-				if (sum < m * n) {
-					printf(" ");
-				}
-			}
-		}
-
-		start++;
-		column--;
-		row--;
-	}
-	return 0;
+    while(start < column && start < row) {
+        if (n == 1) {
+            for (int i = 0; i < m; i++) {
+                if (sum < m * n) {
+                    printf("%d", matrix[i][0]);
+                }
+                sum ++;
+                if (sum < m * n) {
+                    printf(" ");
+                }
+            }
+        } else if (m == 1) {
+            for (int j = 0; j < n; j++) {
+                printf("%d", matrix[0][j]);
+                sum ++;
+                if (sum < m * n) {
+                    printf(" ");
+                }
+            }
+        } else {
+            for (int j = start; j < column; j++) {
+                if (sum < m * n) {
+                    printf("%d", matrix[start][j]);
+                    sum ++;
+                }
+                if (sum < m * n) {
+                    printf(" ");
+                }
+            }
+            
+            for (int i = start + 1; i < row; i++) {
+                if (sum < m * n) {
+                    printf("%d", matrix[i][column-1]);
+                    sum ++;
+                }
+                if (sum < m * n) {
+                    printf(" ");
+                }
+            }
+            
+            for (int j = column-2; j >= start; j--) {
+                if (sum < m*n) {
+                    printf("%d", matrix[row-1][j]);
+                    sum ++;
+                }
+                if (sum < m * n) {
+                    printf(" ");
+                }
+            }
+            
+            for (int i = row-2; i > start; i--) {
+                if (sum < m * n) {
+                    printf("%d", matrix[i][start]);
+                    sum ++;
+                }
+                if (sum < m * n) {
+                    printf(" ");
+                }
+            }
+        }
+        start++;
+        column--;
+        row--;
+    }
+    return 0;
 }
