d53e2ccc21029b1e66293cc313c71704b6a2b667
Kong Weihan <2830524879@qq.com>
2020-06-30 16:10:43 +0800

    Modified task 8

--- 8/8.c
+++ 8/8.c
@@ -20,8 +20,12 @@ int main()
     char largest[101] = {'\0'};
     
     for (i = 0; i < n; i++)
-        for(j = 0; scanf("%c", &name[i][j]) != EOF && name[i][j] != '\n'; j++);
-        	name[i][j]='\0';
+        for(j = 0; scanf("%c", &name[i][j]) != EOF; j++) {
+        	if(name[i][j] == '\n') {
+        		name[i][j]='\0';
+        		break;
+			}	
+		} 	
         	
     for (strcpy(largest, name[0]), i = 1; i < n; i++)
         if (strlen(largest) < strlen(name[i]))
